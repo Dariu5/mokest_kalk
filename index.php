fc5efdaabf0416f044565cb6ee897ac814b9ee07
root <darius@radiocool.lt>
2016-03-17 20:04:04 +0200

    skaiciuoja mokescius

--- index.php
+++ index.php
@@ -12,21 +12,46 @@
 <body>
 
 <?php
-
+session_start();
 $komerc_pvm_nuo = 22;
 $ne_komerc_pvm_nuo =45;
 
 $muito_mokest_nuo =150;
 $past_ne_komer_muito_mokestis_iki =700;
 
+$PVM_tarifas = 21;
+$pastovus_muito_tarifas = 2.5;
+$tarpininko_mokestis = 8.69;
+
 $PVM_taikomas = false;
+$tarpininkas_taikomas = false;
 $muitas_taikomas = 0;
 
-$p_verte = $_POST['p_verte'];
-$s_verte = $_POST['s_verte'];
-$komercine = $_POST['ar_komercine'];
+if (!empty($_POST['p_verte']))
+	
+	$_SESSION["p_verte"] = $_POST['p_verte'];
+			
+	
+	
+if (!empty($_POST['s_verte']))
+	$_SESSION["s_verte"]= $_POST['s_verte'];
+
+
+if (!empty($_POST['ar_komercine']))
+	$_SESSION["ar_komercine"]= $_POST['ar_komercine'];
+
+$p_muito_tarifas = $_POST['p_muito_tarifas'];
+
+$p_verte =$_SESSION["p_verte"];
+$s_verte =$_SESSION["s_verte"];
+$komercine =$_SESSION["ar_komercine"];
+
 $sent = $_POST['sent'];
-$suma = $p_verte + $s_verte;
+
+
+$muito_mokestis =0;
+$PVM_mokestis=0;
+$viso_mokesciu=0;
 ?>
 
  <script type="text/javascript" src="check.js"></script>
@@ -41,7 +66,7 @@ echo '
   <br>
   Ar siunta yra komercinio pobūdžio?:<br>';
   
-  if ($sent == 1)
+  if ($sent > 0)
 	  
 	  {
 		  if ($komercine == 'taip' )
@@ -67,7 +92,7 @@ echo '
    <input type="hidden" name="sent" value="1"><br>
 </form>';
 
- if ($sent == 1) {
+ if ($sent > 0) {
 	 
 	 // ar taikomas PVM?
 	 if ($komercine == 'taip') 
@@ -99,10 +124,6 @@ if ($PVM_taikomas == true)
 
 {
 $PVM_taikomas_string = '<td bgcolor="#FF0000">Taip</td>';
-$tarpininkas_taikomas_string = '<td bgcolor="#FF0000">Taip</td>';
-$tarpininkas_taikomas_komentaras_string = 'Kadangi siuntai priskaičiuoti importo mokesčiai Lietuvos paštas ima atlygį už muitinės tarpininko paslaugą.';
-
-
 
 if ($komercine == 'taip')
 	
@@ -117,11 +138,6 @@ else
 {	
 $PVM_taikomas_string ='<td bgcolor="#00FF00">Ne</td>';
 $PVM_komentaras_string ='';
-
-$tarpininkas_taikomas_string ='<td bgcolor="#00FF00">Ne</td>';
-$tarpininkas_taikomas_komentaras_string ='';
-
-
 }
 
 // formuojamas muito komentaras lentelėje		
@@ -144,6 +160,21 @@ if ($muitas_taikomas > 0)
 		
 	}
 
+// Ar taikomas tarpiniko mokestis?
+
+if (($PVM_taikomas)||($muitas_taikomas))
+	$tarpininkas_taikomas = true;
+else $tarpininkas_taikomas = false;
+
+if ($tarpininkas_taikomas)
+	
+	{$tarpininkas_taikomas_string = '<td bgcolor="#FF0000">Taip</td>';
+	$tarpininkas_taikomas_komentaras_string = 'Kadangi siuntai priskaičiuoti importo mokesčiai Lietuvos paštas ima atlygį už muitinės tarpininko paslaugą.';}
+	
+	else
+		
+{$tarpininkas_taikomas_string ='<td bgcolor="#00FF00">Ne</td>';
+$tarpininkas_taikomas_komentaras_string ='';}
 	
 echo '
 <table width="500">
@@ -167,7 +198,113 @@ echo '
     '.$tarpininkas_taikomas_string.'
     <td>'.$tarpininkas_taikomas_komentaras_string.'</td>
   </tr>
-</table>
-';
- }
+</table><br>';
+
+if ($muitas_taikomas == 2)
+	
+	{
+		echo'
+		<br>
+		<b> Nurodykite muito tarifą, %</b>
+		<form name="kalkuliatorius" action="index.php" method="post" onsubmit="return validateForm()">
+		<input type="text" name="p_muito_tarifas" id ="iii" ><br>
+		<br>
+		<input type="submit" class="button" value="Skaičiuoti"><br>
+		<br>
+		<input type="hidden" name="sent" value="2"><br>
+		</form>
+		';
+	}
+ 
+ 
+ if (($muitas_taikomas <= 1)||($sent ==2)) {
+ 
+ 
+ if (($PVM_taikomas== true)||($muitas_taikomas >0 )|| ($tarpininkas_taikomas==true)) 
+	 
+	 {
+
+
+	 if ($muitas_taikomas== 1)
+			
+			{
+				
+				$muito_mokestis =  $p_verte * $pastovus_muito_tarifas/100;
+				
+			}
+			
+			 if ($muitas_taikomas== 2)
+			
+			{
+				
+				$muito_mokestis =  $p_verte * $p_muito_tarifas/100;
+				
+			}
+	 
+		if ($PVM_taikomas== true)
+			
+			{
+				
+				$PVM_mokestis = ($p_verte + $s_verte + $muito_mokestis + $tarpininko_mokestis) *$PVM_tarifas/100; 
+				
+			}
+		
+			
+			
+
+$viso_mokesciu = $muito_mokestis + $PVM_mokestis + $tarpininko_mokestis;
+
+	 echo '<table width="500">
+  <tr>
+    <th>Mokestis</th>
+    <th>Suma</th>
+    <th>Komentaras</th>
+  </tr>';
+  
+  if ($muitas_taikomas >0)
+	  
+	  { echo '
+		 <tr>
+    <td>Muito</td>
+    <td>'.$muito_mokestis.'</td>
+    <td></td>
+	</tr>
+	 '
+		  	  ;}  
+ if ($PVM_taikomas)
+	  
+	  { echo '
+		 <tr>
+    <td>PVM</td>
+    <td>'.$PVM_mokestis.'</td>
+    <td></td>
+	</tr>
+	 '
+		  	  ;} 
+
+if ($tarpininkas_taikomas)
+	  
+	  { echo '
+		 <tr>
+    <td>Muitinės tarpininko</td>
+    <td>'.$tarpininko_mokestis.'</td>
+    <td></td>
+	</tr>
+	 '
+		  	  ;}
+   
+   echo '
+   
+   <tr>
+    <td>Viso:</td>
+    <td>'.$viso_mokesciu.'</td>
+    <td></td>
+	</tr>
+   
+</table>';
+		 
+		 
+	 } 
+ }}
  ?>
+ </body>
