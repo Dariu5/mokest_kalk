96c06aa301deb52df1bf568da481f9e69160e9ca
root <darius@radiocool.lt>
2016-04-10 22:02:32 +0300

    responsive layout

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
   <title>Importo mokesčių kalkuliatorius</title>
   <meta name="description" content="Importo mokesčių skaičiuoklė">
   <meta name="author" content="radiocool.lt">
+  <meta name="viewport" content="width=device-width" />
 
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
 <?php
-locale_set_default('de-DE');
+locale_set_default('lt-LT');
 session_start();
+
 $komerc_pvm_nuo = 22;
 $ne_komerc_pvm_nuo =45;
 
@@ -40,11 +42,13 @@ if (!empty($_POST['s_verte']))
 if (!empty($_POST['ar_komercine']))
 	$_SESSION["ar_komercine"]= $_POST['ar_komercine'];
 
-$p_muito_tarifas =(float)str_replace(",",".",$_POST['p_muito_tarifas']);
+if (!empty($_POST['p_muito_tarifas']))
+$_SESSION["p_muito_tarifas"] =(float)str_replace(",",".",$_POST['p_muito_tarifas']);
 
 $p_verte =$_SESSION["p_verte"];
 $s_verte =$_SESSION["s_verte"];
 $komercine =$_SESSION["ar_komercine"];
+$p_muito_tarifas = $_SESSION["p_muito_tarifas"]; 
 
 $sent = $_POST['sent'];
 
@@ -64,11 +68,12 @@ function formatted($value) {
 ?>
 
  <script type="text/javascript" src="check.js"></script>
-
-<div class ="header">
-
+<div id = "wrapper">
+<div class = "header">
+<div id = "header_footer_text">
 <h1>Importo mokesčių skaičiuoklė</h1>
 <h2>Skaičiuoklė apskaičiuoja mokesčius taikomus pašto siuntoms iš trečiųjų šalių</h2>
+</div>
 </div>
 
 <div class = "main"> 
@@ -79,7 +84,7 @@ function formatted($value) {
 		</div>
 		<div class="forma">
 	
-			<form name="kalkuliatorius" action="index.php" method="post" id="input_area" onsubmit="return validateForm(1)">
+			<form name="kalkuliatorius" action="index.php#taikomi" method="post" id="input_area" onsubmit="return validateForm(1)">
 							
 			<?php
 			echo '
@@ -213,11 +218,12 @@ $tarpininkas_taikomas_komentaras_string ='';}
 	
 echo '
 <div id = "taikomi_mokesciai">
+<a name="taikomi"></a>
 <div class = "area_header">
 		<h3>Kokie mokesčiai taikomi Jūsų siuntai</h3>
 		</div>
-<div class = "table" >
-<table width="500" class="tg">
+
+<table class="tg">
   <tr>
     <th class="tg-yw4l">Mokestis</th>
     <th class="tg-yw4l">Ar taikomas</th>
@@ -239,7 +245,7 @@ echo '
     <td class="tg-yw4l">'.$tarpininkas_taikomas_komentaras_string.'</td>
   </tr>
 </table>
-</div></div>
+</div>
 ';
 
 if ($muitas_taikomas == 2)
@@ -251,8 +257,8 @@ if ($muitas_taikomas == 2)
 		<h3>Įveskite muito dydį</h3>
 		</div>
 			<div class= "forma">
-			<form name="muito_tarifas" action="index.php" method="post" onsubmit="return validateForm(2)">
-			<span><p>Siuntai taikomas muito tarifas, kuris priklauso nuo siuntėjo šalies, bei prekių rūšies.</p><p>Tarifą galima rasti  <a href = "http://litarweb.cust.lt/taric/web/main_LT" target="_blank">  Lietuvos muitinės svetainėje</a>.</p></span>
+			<form name="muito_tarifas" action="index.php#moketi" method="post" onsubmit="return validateForm(2)">
+			<span><p>Tolesniems skaičiavimams reikia nurodyti taikomą muito tarifą. Jis priklauso nuo siuntėjo šalies, bei prekių rūšies.</p><p>Tarifą galima rasti  <a href = "http://litarweb.cust.lt/taric/web/main_LT" target="_blank">  Lietuvos muitinės svetainėje</a>.</p></span>
 			<span><label>Nurodykite muito tarifą, %:</label></span>
 			<span><input type="text" name="p_muito_tarifas" class="input" id ="iii" value = "'.formatted($p_muito_tarifas).'"></span>
 			<div class = "submit">
@@ -305,11 +311,12 @@ $viso_mokesciu = $muito_mokestis + $PVM_mokestis + $tarpininko_mokestis;
 
 	 echo '
 	 <div id = "paskaiciuoti_mokesciai">
+	 <a name="moketi"></a> 
 	 <div class = "area_header">
 		<h3>Kiek reikės mokėti</h3>
 		</div>
 	<div class = "table">
-	 <table width="500" class="tg">
+	 <table class="tg">
   <tr>
     <th class="tg-yw4l">Mokestis</th>
     <th class="tg-yw4l">Komentaras</th>
@@ -387,8 +394,10 @@ if ($tarpininkas_taikomas)
   
  </div>
  <div class = "footer">
- <p> Ši skaičiuoklė yra rekomendacinė ir jos pateikti duomenis nebūtinai sutaps su mokėtina suma.<p/>
+ <div id = "header_footer_text">
+ <p>Ši skaičiuoklė yra rekomendacinė ir jos pateikti duomenis nebūtinai sutaps su mokėtina suma.<p/>
+ <p>Skaičiavimams naudojamas Lietuvos pašto muitinės tarpininko mokestis, siunčiant per kurjerius (UPS, DHL, Fedex ir kitus) jis bus didesnis.<p/>
  <p> <a href="http://www.radiocool.lt/gidas-apie-importomuito-mokescius-perkantiems-prekes-pastu-is-ne-es-saliu/" target="_blank">Gidas apie importo(muito) mokesčius perkantiems prekes paštu iš ne ES šalių.<a/><p/>
  <p> <a href = "http://goo.gl/forms/8AMzQTvAX8" target="_blank">Susiekite su skaičiuoklės kūrėju</a>, jeigu pastebėjote klaidų, arba turite pasiūlymų skaičiuoklei.<p/>
- </div>  
+ </div></div></div> 
  </body>
